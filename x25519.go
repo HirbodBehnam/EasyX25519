86c7cb67d0730946e3d543e0d0a0b2d88841ee01
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2020-03-08 08:35:58 +0330

    Fixed some comment styles

--- x25519.go
+++ x25519.go
@@ -1,4 +1,4 @@
-// a small helper library to make X25519 key agreement algorithm easier
+// A small helper library to make X25519 key agreement algorithm easier
 package x25519
 
 import (
@@ -6,23 +6,23 @@ import (
 	"golang.org/x/crypto/curve25519"
 )
 
-// the structure that holds public key and secret key. Share public key to perform key agreement. Do not share the secret key
+// The structure that holds public key and secret key. Share public key to perform key agreement. Do not share the secret key
 type KeyPair struct {
 	PublicKey []byte // share this value with anyone you like!
 	SecretKey []byte // keep this one secret
 }
 
-// generates a public key and a secret for key agreement
-func NewX25519() (*KeyPair,error) {
+// Generates a public key and a secret for key agreement
+func NewX25519() (*KeyPair, error) {
 	// read more: https://cr.yp.to/ecdh.html
 	// create a new key structure
 	key := &KeyPair{
-		PublicKey: make([]byte,32), // both public and private keys are 32 bytes
-		SecretKey: make([]byte,32), // both public and private keys are 32 bytes
+		PublicKey: make([]byte, 32), // both public and private keys are 32 bytes
+		SecretKey: make([]byte, 32), // both public and private keys are 32 bytes
 	}
 	// randomly fill secret key with crypto/rand
 	_, err := rand.Read(key.SecretKey)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	// as defined in https://cr.yp.to/ecdh.html do these operation to finalize the private key
@@ -30,31 +30,31 @@ func NewX25519() (*KeyPair,error) {
 	key.SecretKey[31] &= 127
 	key.SecretKey[31] |= 64
 	// compute the public key
-	key.PublicKey, err = curve25519.X25519(key.SecretKey,curve25519.Basepoint)
-	if err != nil{
+	key.PublicKey, err = curve25519.X25519(key.SecretKey, curve25519.Basepoint)
+	if err != nil {
 		return nil, err
 	}
 	// return the key
 	return key, nil
 }
 
-// generate a key pair from private key
-func NewX25519FromPrivateKey(privateKey []byte) (*KeyPair,error) {
+// Generate a key pair (public key) from private key
+func NewX25519FromPrivateKey(privateKey []byte) (*KeyPair, error) {
 	key := &KeyPair{
 		PublicKey: nil,
 		SecretKey: privateKey,
 	}
 	// compute the public key
 	var err error
-	key.PublicKey, err = curve25519.X25519(key.SecretKey,curve25519.Basepoint)
-	if err != nil{
+	key.PublicKey, err = curve25519.X25519(key.SecretKey, curve25519.Basepoint)
+	if err != nil {
 		return nil, err
 	}
 	// return the key
 	return key, nil
 }
 
-// calculates a shared secret given with the other user's public key
-func (key *KeyPair) GenerateSharedSecret(otherPublicKey []byte) ([]byte,error) {
+// Calculates a shared secret given with the other user's public key
+func (key *KeyPair) GenerateSharedSecret(otherPublicKey []byte) ([]byte, error) {
 	return curve25519.X25519(key.SecretKey, otherPublicKey)
 }
